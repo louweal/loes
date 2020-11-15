97b607835269384ed6cc061a49ae51001d32c200
louweal <anneloeslouwe@live.nl>
2020-11-15 14:20:21 +0100

    changed nav and deleted mobile splash screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 function showMenu(elem) {
     var menu = document.getElementById("menu");
-    menu.classList.remove("hideOnDesktop");
+    menu.classList.remove("hide");
 
     var menuIcon = document.getElementById("menu-icon");
     menuIcon.classList.add("hide");
@@ -8,7 +8,7 @@ function showMenu(elem) {
 
 function hideMenu(elem) {
     var menu = document.getElementById("menu");
-    menu.classList.add("hideOnDesktop");
+    menu.classList.add("hide");
 
     var menuIcon = document.getElementById("menu-icon");
     menuIcon.classList.remove("hide");
